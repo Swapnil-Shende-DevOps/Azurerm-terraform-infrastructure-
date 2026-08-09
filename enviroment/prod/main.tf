837f4eb3155b7e48aed27190ab98ee633ecfef16
swapnilshende-git <shendeswapnil1986@gmail.com>
2026-08-10 01:23:42 +0530

    feat: add resource group, virtual network, and subnet modules to production environment configuration

--- enviroment/prod/main.tf
+++ enviroment/prod/main.tf
@@ -1,17 +1,17 @@
 module "resource_group" {
-  source = "../../modules/Azurerm_resource_group"
+  source = "../modules/Azurerm_resource_group"
   rgs    = var.rgs
 }
 
 module "virtual_network" {
   depends_on = [module.resource_group]
-  source     = "../../modules/Azurerm_virtual_network"
+  source     = "../modules/Azurerm_virtual_network"
   vnet       = var.vnet
 }
 
 module "subnet" {
   depends_on = [module.virtual_network]
-  source     = "../../modules/Azurerm_subnet"
+  source     = "../modules/Azurerm_subnet"
   subnet     = var.subnet
 }
 
