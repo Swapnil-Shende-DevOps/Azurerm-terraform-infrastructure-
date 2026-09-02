e1513a48c46e65daa44dbd3dd26344b97cf537ff
swapnilshende-git <shendeswapnil1986@gmail.com>
2026-09-03 01:27:07 +0530

    new pipeline

--- enviroment/prod/main.tf
+++ enviroment/prod/main.tf
@@ -3,12 +3,12 @@ module "resource_group" {
   rgs    = var.rgs
 }
 module "Storage_account" {
-  depends_on =[module.resource_group]
-  source = "../modules/stg"
- stg  = var.stg
- location = var.location
- RG = var.RG
-  account_tier = var.account_tier
-account_replication_type =var.account_replication_type
+  depends_on               = [module.resource_group]
+  source                   = "../modules/stg"
+  stg                      = var.stg
+  location                 = var.location
+  RG                       = var.RG
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
 }
 
